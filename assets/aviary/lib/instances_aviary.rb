99a8380dce06859c67206b67733bb2579cea12a7
Eric Malm <emalm@pivotallabs.com>
2014-03-21 08:47:40 -0700

    Instance aviary delegates to separate CF and pinged aviaries

--- assets/aviary/lib/instances_aviary.rb
+++ assets/aviary/lib/instances_aviary.rb
@@ -1,6 +1,8 @@
 require "cfoundry"
 require "net/http"
 require_relative "instance_pinger"
+require_relative "instances_pinged_aviary"
+require_relative "instances_from_cf_aviary"
 
 class InstancesAviary
   def initialize(target, domain, user, password, org, space, app_name, instance_count)
@@ -9,38 +11,23 @@ class InstancesAviary
     @app_name, @instance_count = app_name, instance_count
   end
 
-  def client
-    @client ||= CFoundry::Client.get(@target).tap do |c|
-      c.login(username: @user, password: @password)
-      c.current_organization = c.organization_by_name(@org)
-      c.current_space = c.space_by_name(@space)
-    end
-  end
-
   def error_message
-    "Instances canary croaked (cfoundry running ratio: #{cfoundry_running_ratio}%, pinged running ratio: #{instance_pinger.running_ratio}%)"
+    "Instances canary croaked (cfoundry running ratio: #{cfoundry_running_ratio}, pinged running ratio: #{pinged_running_ratio})"
   end
 
   def ok?
-    cfoundry_running_ratio >= 0.8 && pinged_running_ratio >= 0.8
+    @pinged_aviary = InstancesPingedAviary.new(@app_name, @domain, @instance_count)
+    @cf_aviary = InstancesFromCFAviary.new(@target, @user, @password, @org, @space, @app_name)
+    cf_aviary_ok = @cf_aviary.ok?
+    pinged_aviary_ok = @pinged_aviary.ok?
+    pinged_aviary_ok && cf_aviary_ok
   end
 
   def cfoundry_running_ratio
-    return app.running_instances.to_f  / app.total_instances
+    @cf_aviary.running_ratio
   end
 
   def pinged_running_ratio
-    instance_pinger.ping!
-    instance_pinger.running_ratio
-  end
-
-  private
-
-  def app
-    @app ||= client.app_by_name(@app_name)
-  end
-
-  def instance_pinger
-    @instance_pinger ||= InstancePinger.new(@app_name, @domain, @instance_count)
+    @pinged_aviary.running_ratio
   end
 end
--- assets/aviary/spec/instances_aviary_spec.rb
+++ assets/aviary/spec/instances_aviary_spec.rb
@@ -13,63 +13,77 @@ describe InstancesAviary do
       100)
   end
 
-  let(:client) { double('cfoundry_client', app_by_name: app).as_null_object }
-  let(:app) { double('app', total_instances: 100, running_instances: 80, url: 'fake_url') }
-  let(:instance_pinger) { double('instance_pinger', running_ratio: 0.25, ping!: 0.25) }
+  let(:cf_aviary) { double(InstancesFromCFAviary, ok?: true, running_ratio: 0.95) }
+  let(:pinged_aviary) { double(InstancesPingedAviary, ok?: true, running_ratio: 0.85) }
 
   before do
-    allow(InstancePinger).to receive(:new).with('fake_app', 'fake_domain', instance_of(Fixnum)).and_return(instance_pinger)
-    allow(CFoundry::Client).to receive(:get).and_return(client)
-  end
-
-  describe '#cfoundry_running_ratio' do
-    it 'returns the correct ratio' do
-      aviary.cfoundry_running_ratio.should == 0.8
-    end
-  end
-
-  describe '#pinged_running_ratio' do
-    it 'delegates to an InstancePinger for its app' do
-      expect(aviary.pinged_running_ratio).to eq 0.25
-      expect(InstancePinger).to have_received(:new).with('fake_app', 'fake_domain', instance_of(Fixnum))
-    end
-  end
-
-  describe '#client' do
-    it 'creates the client only once' do
-      expect(CFoundry::Client).to receive(:get).once
-      2.times { aviary.client }
-    end
+    allow(InstancesPingedAviary).to receive(:new).with('fake_app', 'fake_domain', instance_of(Fixnum)).and_return(pinged_aviary)
+    allow(InstancesFromCFAviary).to receive(:new).and_return(cf_aviary)
   end
 
   describe '#ok?' do
-    context 'when at least 80% of the instances can be pinged' do
-      before do
-        allow(instance_pinger).to receive(:running_ratio).and_return(0.80)
-      end
+    it 'creates new pinged and cf aviaries and calls their ok? methods' do
+      aviary.ok?
+      expect(InstancesPingedAviary).to have_received(:new)
+      expect(InstancesFromCFAviary).to have_received(:new)
+      expect(pinged_aviary).to have_received(:ok?)
+      expect(cf_aviary).to have_received(:ok?)
+    end
 
-      context 'and cloud controller reports at least 80% of the instances are up' do
+    context 'when the pinged aviary is ok' do
+      context 'and the CF aviary is ok' do
         it 'is ok' do
           expect(aviary).to be_ok
         end
       end
 
-      context 'but cloud controller reports less than 80% of the instances are up' do
+      context 'but the CF aviary is not ok' do
         it 'is not ok' do
-          allow(app).to receive(:running_instances).and_return(20)
+          allow(cf_aviary).to receive(:ok?).and_return(false)
           expect(aviary).not_to be_ok
         end
       end
     end
 
-    context 'when less than 80% of the instances can be pinged' do
+    context 'when the pinged aviary is not ok' do
       before do
-        allow(instance_pinger).to receive(:running_ratio).and_return(0.10)
+        allow(pinged_aviary).to receive(:ok?).and_return(false)
       end
 
       it 'is not ok' do
         expect(aviary).not_to be_ok
       end
+
+      context 'but the CF aviary is ok' do
+        it 'is not ok' do
+          expect(aviary).not_to be_ok
+        end
+      end
+    end
+  end
+
+  describe '#error_message' do
+    it 'reports both the CF and pinged ratios after a check' do
+      aviary.ok?
+      error_message = aviary.error_message
+      expect(error_message).to match(/cfoundry running ratio: 0.95/)
+      expect(error_message).to match(/pinged running ratio: 0.85/)
+    end
+  end
+
+  describe '#cfoundry_running_ratio' do
+    it 'returns the running ratio from the CF aviary after a check' do
+      allow(cf_aviary).to receive(:running_ratio).and_return(0.90)
+      aviary.ok?
+      expect(aviary.cfoundry_running_ratio).to eq 0.90
+    end
+  end
+
+  describe '#pinged_running_ratio' do
+    it 'returns the running ratio from the pinged aviary after a check' do
+      allow(pinged_aviary).to receive(:running_ratio).and_return(0.70)
+      aviary.ok?
+      expect(aviary.pinged_running_ratio).to eq 0.70
     end
   end
 end
